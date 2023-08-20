31efdc50dc720012afa86907e222f5e85723ffd7
Elnatan Emanuel Lima Santos <you@example.com>
2023-08-19 22:25:37 -0300

    Create Services

---
+++ src/app/shared/services/snackbar/snackbar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackbarService {
+
+  constructor(private _snackBar: MatSnackBar,) { }
+
+  openSnackBar(msg:string, type:string) {
+    this._snackBar.open(msg, '', {
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      duration:5000,
+       panelClass: [`snackbar-type-soft-${type}`]
+
+    });
+  }
+}
